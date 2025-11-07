5394d1f7dd232c2ebe1fc4bc03d22c3f80de4b93
Reyli Santamaria <156019052+reylisantamaria@users.noreply.github.com>
2025-11-07 02:03:37 -0800

    Removed legacy code & outdated Readme

--- src/legacy/Laser.h
+++
@@ -1,7 +0,0 @@
-#include "GameObject.h"
-
-class Laser : public GameObject {
-  public:
-    Laser(SDL_Renderer* renderer, const char* path, float x, float y, float w, float h)
-      : GameObject(renderer, path, x, y, w, h) {}
-};
--- src/legacy/Meteor.h
+++
@@ -1,7 +0,0 @@
-#include "GameObject.h"
-
-class Meteor : public GameObject {
-  public:
-    Meteor(SDL_Renderer* renderer, const char* path, float x, float y, float w, float h)
-      : GameObject(renderer, path, x, y, w, h) {}
-};
